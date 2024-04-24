f39917558f43a58332466386eda5cb8bfa8f269c
unknown <nurlanmu@code.edu.az>
2024-04-24 16:20:52 +0400

    add practise

---
+++ assets/js/script.js
@@ -0,0 +1,201 @@
+
+"use strict";
+
+
+//practise
+
+// var address = "Ehmedli";
+
+// console.log(address)
+
+// let phone = "438472384732984";
+
+// console.log(phone)
+
+// const email = "test@gmail.com";
+
+// console.log(email)
+
+
+
+
+
+// let a =5;
+// a++;
+// console.log(a)
+// console.log(a/0)
+
+// let b=10;
+
+// let result = a+ b;
+
+// console.log(result)
+
+
+
+
+
+// let isMarried = true;
+
+// if(isMarried=true){
+//     console.log("evlidir")
+// }
+// else{console.log("subaydir")}
+
+
+
+
+// let number = 100;
+
+// if(number>50 && number<200){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+
+
+
+
+
+// let weekDay = "Wednesday";
+
+// switch (weekDay) {
+//     case "Tuesday":
+//         console.log("Tuesday")
+//         break;
+//     case "Wednesday":
+//         console.log("Wednesday")
+//         break;
+
+//     default:
+//         console.log("nt selected")
+//         break;
+// }   
+
+
+
+// alert("press to button")
+
+
+// confirm("press to button")
+
+
+
+// let person = prompt("please enter your name", "Harry Potter")
+
+
+// if(person==null){
+//     console.log("Operation canceled")
+// }
+// else{
+//     console.log(person)
+// }
+
+
+
+
+
+
+
+
+// let a = 5
+
+
+// while(a<8){
+//     console.log()
+    //a++
+// }
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+
+// let num = 100;
+// let num2= "100";
+
+// console.log(num == num2)
+// console.log(num === num2)
+
+
+// let datas = [1,2,3,4,"salam" , "sagol", true];
+
+// //console.log(datas[4])
+
+// for (let i = 0; i < datas.length; i++) {
+//     if(!isNaN (datas[i]) && typeof datas[i] != 'boolean')
+//     {
+//         console.log(datas[i]) 
+//     } 
+// }
+
+
+
+
+
+// let elem =50;
+
+// let result =    elem > 100 ? "yes" : "no";
+
+// console.log(result)
+
+
+
+
+// let b;
+// let a=null;
+
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+// let numbers = [1,2,3,3,4,5,5,5,5,43,3];
+
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+    
+// }
+
+// console.log(numbers);
+
+
+
+
+// let isTrue = true;
+
+// if(isTrue){
+//     if(true){
+//         let surname = "Fexriyye";
+//     }
+// }
+
+// console.log(surname)
+
+
+// const products = ["sade", "ksjaks"] ;
+
+// for (let i = 0; i < products.length; i++) {
+//       console.log(products[i])
+// }
+
+// const name = "test";
+// name ="test2";
+
+
+
+
+
+
+
